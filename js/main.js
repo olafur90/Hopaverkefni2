41271d6e0f1ef5fb4fe207390783f1aabbc91265
hjortur solvi <hss59@his.is>
2020-11-27 22:04:18 +0000

    FIN

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@
 const titill = document.createElement('h1');
 const warpgate = document.createElement('a');
 
+const foot = document.createElement('p');
+
 warpgate.setAttribute('href', 'video.html');
 warpgate.innerText = 'Shortcut';
 titill.innerText = 'Fræðslumyndbandaleigan';
+foot.innerText = '@ fræðslumyndbandaleigan';
 
 document.body.appendChild(titill);
 document.body.appendChild(warpgate);
+document.body.appendChild(foot);
